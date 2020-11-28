0173f12983ecbd87c497baf394e3105c767d038b
kevinstarry <2552712328@qq.com>
2020-11-28 15:44:05 +0800

    .

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/bash
+
+export http_proxy=http://127.0.0.1:10809
+
+export https_proxy=http://127.0.0.1:10809
+
 hugo && git add . && git commit -m "." && git push  &&  cd public/ && git add . && git commit -m "." && git push origin master:public
