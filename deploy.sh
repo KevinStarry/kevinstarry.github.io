4e373683a8f349ab6d95e1e2f5ddef02264d4727
kevinstarry <2552712328@qq.com>
2020-11-29 19:24:42 +0800

    .

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ fi
 # 推送hugo项目
 hugo  && git add .  && git commit -m "$commit"  && git push
 # 推送生成的public文件
-cd public/ && git commit -m "." && git push origin master:public
+cd public/ && git add . && git commit -m "." && git push origin master:public
