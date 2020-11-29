7c6d30da32f1bb3d9f6530174efbbc0628ef9a98
KevinStarry <33227207+KevinStarry@users.noreply.github.com>
2020-11-29 19:29:04 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/bash
-# 在空文件夹下Git Bash窗口执行下面的这句 克隆命令。完成后执行当前文件，即./init.sh
-# git clone git@github.com:KevinStarry/kevinstarry.github.io.git ./
-
 cd public/ && git init && git remote add origin git@github.com:KevinStarry/kevinstarry.github.io.git && git pull origin public
