2db7fe86c58743e22da1b68a501358b7eb3b9d01
kevinstarry <2552712328@qq.com>
2020-11-29 19:12:53 +0800

    modify *.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/bash
-
-#删除原来的public
-rm -rf public
-
-#写注释的部分
-read -p "输入本次提交的注释（如果为空则默认为.) ：" commit
+read -p "输入本次提交的注释:" commit
 if [ ! -n "$commit" ]; then
 commit="."
 fi
-echo "提交hugo项目"
+
 hugo  && git add .  && git commit -m "$commit"  && git push
-echo "hugo项目提交完毕"
-echo "====================================================="
-echo "把生成的public文件部署到网站上去"
-# 强制推送，会覆盖原有的项目 
-cd public/ && git init && git remote add origin git@github.com:KevinStarry/kevinstarry.github.io.git && git add . && git commit -m "." && git push -f origin master:master
-echo "命令执行完毕"
+
+cd public/ && git commit -m "." && git push origin master:master
---
+++ init.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+git clone git@github.com:KevinStarry/kevinstarry.github.io.git ./ && cd public/ && git init && git remote add origin git@github.com:KevinStarry/kevinstarry.github.io.git && git pull origin public
