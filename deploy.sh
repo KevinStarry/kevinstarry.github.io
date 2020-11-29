b480a7db0088bde4a7f6729dcfedfbabc8a027a0
KevinStarry <33227207+KevinStarry@users.noreply.github.com>
2020-11-29 19:20:45 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
+# 注释部分
 read -p "输入本次提交的注释:" commit
 if [ ! -n "$commit" ]; then
 commit="."
 fi
-
+# 推送hugo项目
 hugo  && git add .  && git commit -m "$commit"  && git push
-
-cd public/ && git commit -m "." && git push origin master:master
+# 推送生成的public文件
+cd public/ && git commit -m "." && git push origin master:public
