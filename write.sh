71f344bdc9ce4b456e49a0a70c2b32f5e2c5f82f
kevinstarry <2552712328@qq.com>
2020-12-14 22:26:49 +0800

    修正写文章脚本

--- write.sh
+++ write.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/bash
 echo "输入文章名称："
 read aritcal
-if [ ! -n "$aritcal" ]; then
-exit
-else 
-    hugo new post/"$artical".md
+if [ -n "$aritcal" ]; then
+    hugo new post/$aritcal.md
+    echo "新文章名称（new post name is)：$aritcal"
 fi
-echo "文章名是：$artical"
