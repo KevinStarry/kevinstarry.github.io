1ce118cb9122192f741f989eecc63dc59a9d56a7
kevinstarry <2552712328@qq.com>
2020-12-15 20:58:56 +0800

    修改writ.sh脚本

--- write.sh
+++ write.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/bash
-echo "输入文章名称："
+echo "文章名称："
 read aritcal
-type="post"
-echo "输入文章类型：1.技术教程 2.个人情绪"
-read num 
-if ((2==${num}))
-then
+
+echo "文章类型：1.技术教程 2.个人情绪"
+read type
+if [ ! -n "$type" ];then
+    type="post"
+fi
+if ((${type}=="2"));then
     type="emotion"
+else
+    type="post"
 fi
 
 if [ -n "$aritcal" ]
 then
     echo "======================================"
     hugo new ${type}/${aritcal}.md
-    echo "新文章名称（new post name is)：$aritcal"
 fi
